f212eb9da8f43f806c7e511176b80ae1eb1edb78
leeves <leeves.chou@gmail.com>
2023-06-02 07:01:21 +0000

    for question15_2

--- question15.sh
+++ question15.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 git clone https://github.com/ian-knight-uofa/git-practice-01
 cd git-practice-01
-git checkout branch1
+git checkout branch1 remote/origin/branch1
 git checkout main
 git merge branch1
