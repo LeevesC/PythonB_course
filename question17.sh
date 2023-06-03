bef7ecdc8cd3ce512592c9e6c4db52eb3e98a344
leeves <leeves.chou@gmail.com>
2023-06-03 03:58:30 +0000

    for ques17

--- question17.sh
+++ question17.sh
@@ -9,14 +9,15 @@ git commit -m 'for main'
 
 git checkout branch1
 mv dir1/dir2/foo dir1
-rm -r dir1/dir2
+rmdir dir1/dir2
 touch newfile1
 git add .
 git commit -m 'for branch1'
 
 git checkout branch2
 mv dir1/dir2/foo dir1/dir2/foo_modified
-rm dir3/bar && touch dir3/newfile2
+rm -r dir3
+mkdir dir1/dir3 && touch dir1/dir3/newfile2
 git add .
 git commit -m 'for branch2'
 
