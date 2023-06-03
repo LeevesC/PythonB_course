bc2e26a4faa9bbd5d03dfb18704fd7632e01172e
leeves <leeves.chou@gmail.com>
2023-06-03 03:05:24 +0000

    for stashing

--- question11.sh
+++ question11.sh
@@ -4,4 +4,5 @@ git checkout branch2
 touch file4
 git add .
 git commit -m 'for file4'
+git stash
 git checkout main
--- question12.sh
+++ question12.sh
@@ -1,4 +1,5 @@
 git checkout branch2
+git stash apply
 git add file4
 
 git commit -m 'question12 update'
